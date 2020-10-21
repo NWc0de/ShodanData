2e3184ea95e2bcf834b325169d04d3a256da5bc0
Spencer Little <Slittle57@gmail.com>
2020-10-20 17:08:29 -0700

    Update documentation, restructure

---
+++ shodan/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# @file /shodan/__init__.py
+# @brief This files serves to designate the 'shodan' dir as a package
+# @author Spencer Little (mrlittle@uw.edu)
+# Last edit: 10/20/2020
--- shodan/query_utils.py
+++ shodan/query_utils.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# @file queryutils.py
+# @file shodan/queryutils.py
 # @brief Provides a set of functions for interacting with the Shodan API
 # @details Wraps the official Shodan python API and enables clients to
 # define queries, retrieve specific sets of attributes from the results,
@@ -156,14 +156,11 @@ def query_shodan(
         unique_ips=True,
         observed_ips=set(),
 ):
-    """Queries the Shodan endpoint with the specified query and
-    returns all results as a list of dictionaries where each dictionary
-    represents a subset (see EXTRACT_ATTRIBUTES) of the attributes returned
-    from Shodan.
+    """Queries the Shodan endpoint with the specified query and returns all results as a list of
+    dictionaries where each dictionary an entity in the response and contains a mapping from the attribute's of interest to Shodan's response for that value, or 'None' if no value was returned from Shodan. Nested attributes are flattened, see extracted_attributes() for more details.
 
-    Shodan returns 100 results per request (or 'page'), so this method uses
-    the page and results attribute to recursively query the endpoint until
-    all results have been retreived.
+    Shodan returns 100 results per request (or 'page'), so this method recursively queries the
+    endpoint until all results have been retreived or the query_limit has been reached.
 
     Args:
         query (str): the search query
